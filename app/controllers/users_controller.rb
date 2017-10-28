5e5e4153eba837457bc2f98636248d2452e36a77
Nelson Dornelas <ndornelasj@gmail.com>
2017-10-28 15:00:29 -0700

    fix: create at user_controller, feat: Use SLL and Puma webserver in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if !@user.save
+    if @user.save
       flash[:success] = "#{@user.name}, welcome to the Sample App!"
       redirect_to @user
     else
