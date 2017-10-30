cbee5fc863ca91a9ab85b925a8b924e389860b67
Nelson Dornelas <ndornelasj@gmail.com>
2017-10-30 12:27:02 -0700

    Implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,9 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # creates a session by retrieving user from db
+  # logs in to the website in case user is true and password authentication is valid
+  # returns error message and re-renders login page otherwise
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
@@ -14,6 +17,7 @@ class SessionsController < ApplicationController
     end
   end
 
+  # detroys the session for logout
   def destroy
     log_out
     redirect_to root_url
