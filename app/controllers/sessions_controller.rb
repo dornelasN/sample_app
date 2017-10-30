85f014821524718522fab03f27014ffe456dfa2e
Nelson Dornelas <ndornelasj@gmail.com>
2017-10-30 11:48:26 -0700

    feat: layout update for logged in user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      #log de user in and redirect to the user's show page
+      log_in user
+      redirect_to user
     else
       # create an error message.
-      flash.now[:danger] = "Invalid email/password combination!" #work to do
+      flash.now[:danger] = "Invalid email/password combination!"
       render 'new'
     end
   end
