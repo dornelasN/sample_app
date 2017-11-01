7228685088d8220f076149cbd34449160428807b
Nelson Dornelas <ndornelasj@gmail.com>
2017-11-01 09:02:16 -0700

    feat: remember-me checkbox and tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,11 @@ class SessionsController < ApplicationController
   # logs in to the website in case user is true and password authentication is valid
   # returns error message and re-renders login page otherwise
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      remember user
-      redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       # create an error message.
       flash.now[:danger] = "Invalid email/password combination!"
