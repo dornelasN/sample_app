3fb00ee4eb49a1a0a56205d7a591ce04d09713a0
Nelson Dornelas <ndornelasj@gmail.com>
2017-11-11 21:28:22 -0800

    reset edit action and view

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,    only: [:edit, :update]
+  before_action :valid_user,  only: [:edit, :update]
+
   def new
   end
 
@@ -15,7 +18,29 @@ class PasswordResetsController < ApplicationController
     end
   end
 
-  # TODO:
   def edit
   end
+
+  #TODO:
+  # consider 4 cases:
+  # 1 - An expired password reset
+  # 2 - A failed update due to an invalid password.
+  # 3 - A failed update (which initially looks 'successful') due to an empty password and confirmation
+  # 4 - A successful update
+  def update
+  end
+
+  private
+
+    def get_user
+      @user = User.find_by(email: params[:email].downcase)
+    end
+
+    # Confirms a valid user
+    def valid_user
+      if (!@user || !@user.activated? || !@user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
+
 end
