b60f96ee353d4c15166cce1e4d9e061141976701
Nelson Dornelas <ndornelasj@gmail.com>
2017-11-14 10:28:41 -0800

    Micropost validation and association with user

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  # validate the presence of user_id in a micropost
+  validates :user_id, presence: true
+  # validates the presence of content attribute in micropost, and its maximum length of 140 chars
+  validates :content, presence: true, length: { maximum: 140 }
 end
--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup 
+    @user = users(:nelson)
+    #using @user.micropost.build already makes the association between the micropost and user
+    #the micropost user_id is set to the @user.id automatically
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
+  end
+
+  test "micropost should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test "content should be present" do
+    @micropost.content = "    "
+    assert_not @micropost.valid?
+  end
+
+  test "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
 end
